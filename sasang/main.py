24a620789cc7cb63d13e94e3d10f7370f274d4fc
Chris Dick <cdick10@gmail.com>
2020-03-29 23:04:28 +0100

    trying to add link url

--- sasang/main.py
+++ sasang/main.py
@@ -1,26 +1,26 @@
 from flask import Flask, redirect, url_for,render_template
-import OpenTokSDK
+from opentok import OpenTok
 
 app = Flask(__name__)
 
 app.config.from_object(__name__)
 
-api_key = ""        # Replace with your OpenTok API key.
-api_secret = ""     # Replace with your OpenTok API secret.
+api_key = "46624262"        # Replace with your OpenTok API key.
+api_secret = "9573c22f4f42ff2aff59ffecac728edb7989c156"     # Replace with your OpenTok API secret.
 
-opentok_sdk = OpenTokSDK.OpenTokSDK(api_key, api_secret)
+opentok_sdk = OpenTok(api_key, api_secret)
 
 @app.route('/')
 def index():
-	session_properties = {OpenTokSDK.SessionProperties.p2p_preference: "disabled"}
-	session = opentok_sdk.create_session(None, session_properties)
+	#session_properties = {OpenTok.SessionProperties.p2p_preference: "disabled"}
+	session = opentok_sdk.create_session(None)#, session_properties)
 	url= url_for('chat',session_id=session.session_id)
 	return redirect(url)
 
 @app.route('/<session_id>')
 def chat(session_id):
  	token=opentok_sdk.generate_token(session_id)
- 	return render_template('chat.html', api_key= api_key, session_id=session_id, token=token)
+ 	return render_template('index.html', api_key= api_key, session_id=session_id, token=token)
 
 if __name__ == '__main__':
-	app.run()
+	app.run(debug=True)
